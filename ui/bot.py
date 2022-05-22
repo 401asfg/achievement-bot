637831529c99dfba85ab9cdadd37260952c2a33e
michaelallan <mb.allan@ymail.com>
2022-05-22 14:20:33 -0700

    Modified guild manager methods to take strings instead of discord members to make the class more testable; wrote some of guild manager's query guild tests; modified bot to support guild_manager changes

--- ui/bot.py
+++ ui/bot.py
@@ -1,4 +1,5 @@
 import os
+from typing import List
 
 import discord
 from discord.ext import commands
@@ -23,23 +24,37 @@ async def on_message(message):
     await bot.process_commands(message)
 
 
+def get_member_ids(members: List[discord.Member]) -> List[str]:
+    """
+    :param members: The members to get the ids of
+    :return: A list of member ids that correspond to the given list of members
+    """
+    return [member.id for member in members]
+
+
 @bot.command(name=ACHIEVE_NAME, help=ACHIEVE_HELP, brief=ACHIEVE_DESCRIPTION)
 async def achieve_command(ctx, member: discord.Member, achievement_name: str):
+    member_id = member.id
+    members = get_member_ids(ctx.guild.members)
+
     try:
-        guild_manager.add_achievement(member, ctx.guild.members, achievement_name)
+        guild_manager.add_achievement(member_id, members, achievement_name)
     except ValueError:
-        await ctx.send(member_not_in_server_error_msg(member.nick))
+        await ctx.send(member_not_in_server_error_msg(member_id))
     except InventoryContainsItemError:
-        await ctx.send(member_already_has_achievement_error_msg(member.nick, achievement_name))
+        await ctx.send(member_already_has_achievement_error_msg(member_id, achievement_name))
 
 
 @bot.command(name=LIST_NAME, help=LIST_HELP, brief=LIST_DESCRIPTION)
 async def list_command(ctx, member: discord.Member):
+    member_id = member.id
+    members = get_member_ids(ctx.guild.members)
+
     try:
-        achievement_list_msg = guild_manager.build_achievement_list_msg(member, ctx.guild.members)
+        achievement_list_msg = guild_manager.build_achievement_list_msg(member_id, members)
         await ctx.send(achievement_list_msg)
     except ValueError:
-        await ctx.send(member_not_in_server_error_msg(member.nick))
+        await ctx.send(member_not_in_server_error_msg(member_id))
 
 
 bot.run(TOKEN)
