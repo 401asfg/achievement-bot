4d4cf0ba2d6206786feed67b4ab4ce0af925aed2
michaelallan <mb.allan@ymail.com>
2022-07-29 14:16:03 -0700

    Added intents to the bot that allow the both to acquire info on the server's members; Rewrote the guild manager's build achievement list msg method to just produce a list of achievements

--- ui/bot.py
+++ ui/bot.py
@@ -5,17 +5,21 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
+from model.achievement import Achievement
 from utils.bot_output_lib import COMMAND_PREFIX, ACHIEVE_NAME, ACHIEVE_HELP, \
     ACHIEVE_DESCRIPTION, member_already_has_achievement_error_msg, LIST_NAME, \
-    LIST_HELP, LIST_DESCRIPTION
+    LIST_HELP, LIST_DESCRIPTION, member_achievements_header_msg, ACHIEVEMENT_BESTOWER_INDENT
 from model.guild_manager import GuildManager, GuildMember
 from model.inventory.exceptions.inventory_contains_item_error import InventoryContainsItemError
 from model.inventory.inventory import Inventory
 from model.person import Person
 
 load_dotenv()
+intents = discord.Intents.default()
+intents.members = True
+
 TOKEN = os.getenv('TOKEN')
-bot = commands.Bot(command_prefix=COMMAND_PREFIX)
+bot = commands.Bot(command_prefix=COMMAND_PREFIX, intents=intents)
 
 guild_manager = GuildManager(Inventory[Person]())      # TODO: pass a guild created from a file?
 
@@ -44,24 +48,42 @@ def create_guild_members(members: List[discord.Member]) -> List[GuildMember]:
 @bot.command(name=ACHIEVE_NAME, help=ACHIEVE_HELP, brief=ACHIEVE_DESCRIPTION)
 async def achieve_command(ctx, member: discord.Member, *achievement_name_segments: str):
     guild_member = create_guild_member(member)
-    guild_members = create_guild_members(ctx.guild.members)
+    valid_guild_members = create_guild_members(ctx.guild.members)
     achievement_name = " ".join(achievement_name_segments)
 
+    # TODO: remove this test code
+    await ctx.send('Valid Guild Members:')
+    [await ctx.send({valid_guild_member.display_name}) for valid_guild_member in valid_guild_members]
+
     try:
-        guild_manager.add_achievement(guild_member, guild_members, achievement_name, ctx.message.author.id)
+        guild_manager.add_achievement(guild_member, valid_guild_members, achievement_name, ctx.message.author.id)
     except ValueError as e:
         await ctx.send(e)
     except InventoryContainsItemError:
         await ctx.send(member_already_has_achievement_error_msg(guild_member.display_name, achievement_name))
 
 
+def create_achievement_list_msg(achievements: List[Achievement], guild_member: GuildMember) -> str:
+    achievement_list_msg = member_achievements_header_msg(guild_member.display_name)
+
+    for achievement in achievements:
+        achievement_list_msg += "\n" + achievement.name + "\n" + ACHIEVEMENT_BESTOWER_INDENT + achievement.bestower
+
+    return achievement_list_msg
+
+
 @bot.command(name=LIST_NAME, help=LIST_HELP, brief=LIST_DESCRIPTION)
 async def list_command(ctx, member: discord.Member):
     guild_member = create_guild_member(member)
-    guild_members = create_guild_members(ctx.guild.members)
+    valid_guild_members = create_guild_members(ctx.guild.members)
+
+    # TODO: remove this test code
+    await ctx.send('Valid Guild Members:')
+    [await ctx.send({valid_guild_member.display_name}) for valid_guild_member in valid_guild_members]
 
     try:
-        achievement_list_msg = guild_manager.build_achievement_list_msg(guild_member, guild_members)
+        achievements = guild_manager.get_achievements(guild_member, valid_guild_members)
+        achievement_list_msg = create_achievement_list_msg(achievements, guild_member)
         await ctx.send(achievement_list_msg)
     except ValueError as e:
         await ctx.send(e)
