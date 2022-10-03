72ae3836ea5fac4ee9ef8201fff5bb77485019ac
michaelallan <mb.allan@ymail.com>
2022-10-03 01:33:48 -0700

    Finished all guild save system tests

--- test/persistence/test_guild_save_system.py
+++ test/persistence/test_guild_save_system.py
@@ -6,6 +6,7 @@ from src.model.achievement import Achievement
 from src.model.guild import Guild
 from src.model.person import Person
 from src.persistence.guild_save_system import GuildSaveSystem
+from src.persistence.json_writer import JsonWriter
 
 
 class TestGuildSaveSystem(unittest.TestCase):
@@ -59,16 +60,56 @@ class TestGuildSaveSystem(unittest.TestCase):
         self.person_c.add(self.achievement_c)
         self.person_c.add(self.achievement_d)
 
-    def copy_guild_json(self):
-        shutil.copy(self.guild_save_system.JSON_STORE, self.COPY_PATH)
+        self.guild.add(self.person_a)
+        self.guild.add(self.person_b)
+        self.guild.add(self.person_c)
 
-    def test_save(self):
-        self.copy_guild_json()
-        # TODO: finish
+        shutil.move(self.guild_save_system.JSON_STORE, self.COPY_PATH)
+
+    def tearDown(self) -> None:
+        shutil.move(self.COPY_PATH, self.guild_save_system.JSON_STORE)
 
     def test_load(self):
-        pass
-        # TODO: finish
+        try:
+            self.guild_save_system.load()
+            self.fail()
+        except FileNotFoundError:
+            pass
+
+        json_writer = JsonWriter(self.guild_save_system.JSON_STORE)
+        json_writer.write(self.guild)
+
+        actual_guild = self.guild_save_system.load()
+        self.assertEqual(self.guild.to_json(), actual_guild.to_json())
+
+    def test_save(self):
+        def assert_save():
+            self.guild_save_system.save(self.guild)
+            actual_guild = self.guild_save_system.load()
+            self.assertEqual(self.guild.to_json(), actual_guild.to_json())
+
+        # Non Existent
+        assert_save()
+
+        # Empty Guild
+        json_writer = JsonWriter(self.guild_save_system.JSON_STORE)
+        guild_to_overwrite = Guild()
+        json_writer.write(guild_to_overwrite)
+        assert_save()
+
+        # Partial Guild
+        person_to_overwrite_a = Person("Person To Overwrite A")
+        person_to_overwrite_b = Person("Person To Overwrite B")
+        achievement_to_overwrite_a = Achievement("Achievement To Overwrite A", "Other Person")
+        achievement_to_overwrite_b = Achievement("Achievement To Overwrite B", "Other Person")
+
+        person_to_overwrite_a.add(achievement_to_overwrite_a)
+        person_to_overwrite_a.add(achievement_to_overwrite_b)
+        guild_to_overwrite.add(person_to_overwrite_a)
+        guild_to_overwrite.add(person_to_overwrite_b)
+
+        json_writer.write(guild_to_overwrite)
+        assert_save()
 
 
 if __name__ == '__main__':
